1c05184a21cb73857b2f1862483caa195f37760b
vdaviSi <57577277+vdaviSi@users.noreply.github.com>
2021-05-17 20:35:45 -0300

    Add files via upload

---
+++ kalcWeb.js
@@ -0,0 +1,81 @@
+
+//Operações
+
+const igual = document.getElementById("igual");
+const mais = document.getElementById("mais");
+const menos = document.getElementById("menos");
+const multi = document.getElementById("multi");
+const divisor = document.getElementById("divisor");
+
+
+//Numeros
+
+const um = document.getElementById("um");
+const dois = document.getElementById("dois");
+const tres = document.getElementById("tres");
+const quatro = document.getElementById("quatro");
+const cinco = document.getElementById("cinco");
+const seis = document.getElementById("seis");
+const sete = document.getElementById("sete");
+const oito = document.getElementById("oito");
+const nove = document.getElementById("nove");
+const zero = document.getElementById("zero");
+const clear = document.getElementById("clear");
+const ponto = document.getElementById("ponto");
+
+//Elementos das operações
+
+igual.onclick = function(){
+    console.log(igual.innerText);
+}
+mais.onclick = function(){
+    console.log(mais.innerText);
+}
+menos.onclick = function(){
+    console.log(menos.innerText);
+}
+multi.onclick = function(){
+    console.log(multi.innerText);
+}
+divisor.onclick = function(){
+    console.log(divisor.innerText);
+}
+
+//Elementos dos numeros
+
+um.onclick = function(){
+    console.log(um.innerText);
+}
+dois.onclick = function(){
+    console.log(dois.innerText);
+}
+tres.onclick = function(){
+    console.log(tres.innerText);
+}
+quatro.onclick = function(){
+    console.log(quatro.innerText);
+}
+cinco.onclick = function(){
+    console.log(cinco.innerText);
+}
+seis.onclick = function(){
+    console.log(seis.innerText);
+}
+sete.onclick = function(){
+    console.log(sete.innerText);
+}
+oito.onclick = function(){
+    console.log(oito.innerText);
+}
+nove.onclick = function(){
+    console.log(nove.innerText);
+}
+zero.onclick = function(){
+    console.log(zero.innerText);
+}
+clear.onclick = function(){
+    console.log(clear.innerText);
+}
+ponto.onclick = function(){
+    console.log(ponto.innerText);
+}
